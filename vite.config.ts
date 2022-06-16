da8a56a7efd93c9ab9da1e64c65b064e04eaede9
Hccake <chengbohua@foxmail.com>
2022-06-17 07:32:18 +0800

    :hammer: package with declaration-files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
 import { fileURLToPath, URL } from 'url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    dts({
+      staticImport: true,
+      // skipDiagnostics: false,
+      // logDiagnostics: true,
+      insertTypesEntry: true,
+      rollupTypes: true
+    }),
+    vue()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueCropper',
+      fileName: format => `index.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue', 'cropperjs'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+          cropperjs: 'CropperJs'
+        }
+      }
     }
   }
 })
